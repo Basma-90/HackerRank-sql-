15aea127434091208f7ec2a470b211157d97cdf3
Basma Sabry <120141275+basmasabry@users.noreply.github.com>
2023-10-04 20:24:17 +0300

    Create PROBLEM3.sql

---
+++ PROBLEM3.sql
@@ -0,0 +1,5 @@
+SELECT ROUND((t.max_x - t.min_x) + (t.max_y - t.min_y), 4)
+FROM (SELECT MAX(LAT_N) AS max_x, MIN(LAT_N) as min_x, MAX(LONG_W) as max_y, MIN(LONG_W) as min_y FROM STATION) as t;
+/*
+problem link 
+https://www.hackerrank.com/challenges/weather-observation-station-18/problem*/
